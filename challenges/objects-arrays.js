7a250d9480c200e0e89f169ad00375e9e843d29c
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-06-14 11:41:49 -0400

    first commit of the day

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,10 +7,29 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+let tRex = {
+  name: "tyrannosaurus",
+  diet: "carnivorous", 
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous"
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+let steggy = {
+  name:"stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+let velo = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
